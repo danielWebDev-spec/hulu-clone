685ca059fc2c04705185ccb9463be6655dd923aa
Daniel <danielwebdeveloper220@gmail.com>
2020-10-27 23:09:28 -0400

    fetching data from api

--- src/components/VideoCard/VideoCard.js
+++ src/components/VideoCard/VideoCard.js
@@ -1,18 +1,35 @@
 import React from "react";
 import "./VideoCard.css";
+import TextTruncate from "react-text-truncate";
+import ThumbUpSharpIcon from "@material-ui/icons/ThumbUpSharp";
 
 function VideoCard({ movie }) {
+  const base_url = "https://image.tmdb.org/t/p/original/";
+
   return (
     <div className="videoCard">
       <img
-        src="https://images.unsplash.com/photo-1487252502161-75020a813bf0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1320&q=80"
-        alt="unsplash"
+        src={`${base_url}${movie.backdrop_path || movie.poster_path}`}
+        alt="movie poster"
       />
-      <div className="videoCard__info">
-        <p>This is a film about coding</p>
-        <h2>Movie title</h2>
-        <p>Number of likes...</p>
-      </div>
+      <TextTruncate
+        line={1}
+        element="p"
+        textTruncateChild={<a href="#">Read more</a>}
+        text={movie.overview}
+      />
+      <TextTruncate
+        line={1}
+        element="h2"
+        truncateText="..."
+        text={movie.title || movie.original_name}
+      />
+      <p className="videoCard__stats">
+        {movie.media_type && `${movie.media_type} ●`}●{" "}
+        {movie.release_date || movie.first_air_date}
+        <ThumbUpSharpIcon />
+        {movie.vote_count}
+      </p>
     </div>
   );
 }
