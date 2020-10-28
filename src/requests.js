889cb20e65ea25adb2efbbea578d8ee534b1667b
Daniel <danielwebdeveloper220@gmail.com>
2020-10-27 23:08:27 -0400

    creating requests.js

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "6121ba702f7b98f17d11895b813f4d5f";
+
+export default {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchMystery: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+  fetchSciFi: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  fetchWestern: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+  fetchTV: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+};
