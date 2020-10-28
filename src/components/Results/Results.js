be8989fe313e7313492581b929bab36eae86170c
Daniel <danielwebdeveloper220@gmail.com>
2020-10-27 23:08:50 -0400

    fetching results

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,11 +1,27 @@
-import React from "react";
+import { MovieSharp } from "@material-ui/icons";
+import React, { useEffect, useState } from "react";
 import VideoCard from "../VideoCard/VideoCard";
 import "./Results.css";
+import axios from "../../axios";
+import requests from "../../requests";
 
 function Results() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const results = await axios.get(requests.fetchActionMovies);
+      setMovies(results.data.results);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="results">
-      <VideoCard />
+      {movies.map((movie) => (
+        <VideoCard movie={movie} />
+      ))}
     </div>
   );
 }
