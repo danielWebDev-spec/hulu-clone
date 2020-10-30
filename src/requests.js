dd37f7c7bda8809fbd7b05b23bf5a89d359a6b4a
Daniel <danielwebdeveloper220@gmail.com>
2020-10-29 23:59:57 -0400

    react app

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const API_KEY = "6121ba702f7b98f17d11895b813f4d5f";
+const API_KEY = `${process.env.REACT_APP_API_KEY}`;
 
 export default {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
